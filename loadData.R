d71b5ec8c15f11f096b80599287c4b22db544818
Edna Gabriela Hernandez Islas <eghernandezi@gmail.com>
2015-03-04 13:30:22 -0600

    Format

--- loadData.R
+++ loadData.R
@@ -1,6 +1,17 @@
+# Function that loads filtered data for house hold power consumtion plotting
 loadData <- function() {
-    power <- read.table("household_power_consumption.txt", sep=";", na.strings="?", colClasses=c(rep("character", 2), rep("numeric",7)), header = TRUE)
-    power[["Time"]] <- strptime(paste(power[["Date"]], power[["Time"]]), "%d/%m/%Y %H:%M:%S")
+    power <- read.table(
+                "household_power_consumption.txt", 
+                sep=";", 
+                na.strings = "?", 
+                colClasses = c(rep("character", 2), rep("numeric",7)), 
+                header = TRUE)
+    power[["Time"]] <- strptime(
+                        paste(power[["Date"]], power[["Time"]]), 
+                        "%d/%m/%Y %H:%M:%S")
     power[["Date"]] <- as.Date(power[["Date"]], "%d/%m/%Y")
-    subset(power, power$Date == as.Date("2007-02-01") | power$Date == as.Date("2007-02-02"))
+    subset(
+        power, 
+        power$Date == as.Date("2007-02-01") 
+            | power$Date == as.Date("2007-02-02"))
 }
